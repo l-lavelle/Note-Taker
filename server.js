c18794b887c7935a115fcf77a432e6f5f5c089a1
Lauren <lauren.m.lavelle@wmich.edu>
2023-08-14 18:35:05 -0400

    added id to each note with uuid

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 // Initalize express
 const app = express();
@@ -33,7 +34,8 @@ app.post('/api/notes',(req,res)=>{
   if (title && text){
     const newNote = {
       title,
-      text
+      text, 
+      id:uuidv4()
     }
   
   noteData.push(newNote)
@@ -69,12 +71,7 @@ app.listen(PORT, () =>
 
 // to do
 // deploy the entire application to Heroku
-// when click save THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column THEN that note appears in the right-hand column
-// ???WHEN I click on the Write icon in the navigation at the top of the page THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-// The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module
 // readme
 // comments
-// id? uuid package?
+
 // https://github.com/uuidjs/uuid#uuidv4options-buffer-offset
-// create routes folder?
